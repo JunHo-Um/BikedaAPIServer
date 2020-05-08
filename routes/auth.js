fbfa664ffef91a312abe843df4e91aebd9799930
JunHo-Um <djaqoek@gmail.com>
2020-05-08 18:04:10 +0900

    0508

---
+++ routes/auth.js
@@ -0,0 +1,264 @@
+var express = require('express');
+var router = express.Router();
+var util = require('../util');
+var jwt = require('jsonwebtoken');
+var models = require('../models');
+
+// 관리자 로그 인증 및 토큰생성
+router.post('/admin', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.adminId){
+    isValid = false;
+    validationError.errors.adminId = {message:'아이디 미입력!'};
+  }
+  if(!req.body.adminPassword){
+    isValid = false;
+    validationError.errors.adminPassword = {message:'비밀번호 미입력!'};
+  }
+  if(!isValid) return res.json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      id : req.body.adminId
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    var options = {expiresIn: 60*60*24};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+
+// 지점 로그인 인증 및 토큰 생성
+router.post('/branch', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  var brcofcBsnsRgnmb = req.body.brcofcBsnsRgnmb  || '';
+  var brcofcPassword  = req.body.brcofcPassword   || '';
+
+  if( brcofcBsnsRgnmb.length < 1 ){
+    isValid = false;
+    validationError.errors.brcofcBsnsRgnmb = {message:'사업자 등록 번호 미입력!'};
+  }
+
+  if( brcofcPassword.length < 1 ){
+    isValid = false;
+    validationError.errors.brcofcPassword = {message:'비밀번호 미입력!'};
+  }
+
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req, res, next ){
+    var validationError = {
+      name:'ValidationError',
+      errors:{}
+    };
+
+    var brcofcBsnsRgnmb = req.body.brcofcBsnsRgnmb  || '';
+    var brcofcPassword  = req.body.brcofcPassword   || '';
+    models.branch_office.findOne({
+      where : {
+        brcofcBsnsRgnmb : brcofcBsnsRgnmb,
+        brcofcPassword : brcofcPassword
+      }
+    }).then( result => {
+      if( !result ){
+        validationError.errors.notfound = {message:'존재 하지 않는 사업자 번호 또는 비밀번호 오류'};
+        return res.status(400).json(util.successFalse(validationError));
+      }
+      var payload = {
+        brcofcBsnsRgnmb : result.brcofcBsnsRgnmb
+      };
+      var secretOrPrivateKey = process.env.JWT_SECRET;
+      var options = {expiresIn: 60*60*24};
+      jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+        if(err) return res.status(400).json(util.successFalse(err));
+        res.status(200).json(util.successTrue(token));
+      });
+    }).catch( err => {
+      return res.status(400).json(util.successFalse(err));
+    });
+
+  }
+);
+
+// 상점 로그인 인증 및 토큰 생성
+router.post('/store', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.stoBsnsRgnmb){
+    isValid = false;
+    validationError.errors.stoBsnsRgnmb = {message:'사업자 등록 번호 미입력!'};
+  }
+  if(!req.body.stoPassword){
+    isValid = false;
+    validationError.errors.stoPassword = {message:'비밀번호 미입력!'};
+  }
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      stoBsnsRgnmb : req.body.stoBsnsRgnmb
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    var options = {expiresIn: 60*60*24};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+
+// 랴이더 로그인 인증 및 토큰 생성
+router.post('/rider', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.riderCelno){
+    isValid = false;
+    validationError.errors.riderCelno = {message:'휴대폰 번호 미입력!'};
+  }
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      riderCelno : req.body.riderCelno
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    var options = {expiresIn: 60*60*24};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+
+// 관리자 인증 토큰 재생성
+router.post('/refresh-admin', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.adminId){
+    isValid = false;
+    validationError.errors.adminId = {message:'아이디 미입력!'};
+  }
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      adminId : req.body.adminId
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    // var options = {expiresIn: 60*60*24};
+    var options = {expiresIn: 60};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+
+// 지점 인증 토큰 재생성
+router.post('/refresh-branch', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.brcofcBsnsRgnmb){
+    isValid = false;
+    validationError.errors.brcofcBsnsRgnmb = {message:'사업자 등록 번호 미입력!'};
+  }
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      brcofcBsnsRgnmb : req.body.brcofcBsnsRgnmb
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    // var options = {expiresIn: 60*60*24};
+    var options = {expiresIn: 60};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+
+// 상점 인증 토큰 재생성
+router.post('/refresh-store', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.stoBsnsRgnmb){
+    isValid = false;
+    validationError.errors.stoBsnsRgnmb = {message:'사업자 등록 번호 미입력!'};
+  }
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      stoBsnsRgnmb : req.body.stoBsnsRgnmb
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    // var options = {expiresIn: 60*60*24};
+    var options = {expiresIn: 60};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+
+// 라이더 인증 토큰 재생성
+router.post('/refresh-rider', function( req, res, next ){
+  var isValid = true;
+  var validationError = {
+    name:'ValidationError',
+    errors:{}
+  };
+  if(!req.body.riderCelno){
+    isValid = false;
+    validationError.errors.riderCelno = {message:'휴대폰 번호 미입력!'};
+  }
+  if(!isValid) return res.status(400).json(util.successFalse(validationError));
+  else next();
+  },
+  function( req,res,next ){
+    var payload = {
+      riderCelno : req.body.riderCelno
+    };
+    var secretOrPrivateKey = process.env.JWT_SECRET;
+    // var options = {expiresIn: 60*60*24};
+    var options = {expiresIn: 60};
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, token){
+      if(err) return res.status(400).json(util.successFalse(err));
+      res.status(200).json(util.successTrue(token));
+    });
+  }
+);
+module.exports = router;
--- routes/branch.js
+++ routes/branch.js
@@ -1,60 +1,88 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/branch');
+var util = require('../util');
+var models = require('../models');
+var sequelize = require("sequelize");
+var Op = sequelize.Op;
 
-// Bikeda branch index page Branch API Document
+
+// 바이크다 지점 API Document
 router.get('/', function( req, res, next ) {
   res.render('branch', { title: '바이크다 지점 API' });
 });
 
-// Bikeda branch login
-router.get('/branch-login/:bsns_rgnmb/:password', function( req, res, next ) {
-  var branches = global.branches;
-  var bsns_rgnmb = req.params.bsns_rgnmb;
-  var password = req.params.password;
-  if( branches.filter( branch => branch.bsns_rgnmb == bsns_rgnmb ).length < 1 ) {
-    return res.status(400).json({err: '등록되지 않은 지점 사업자 등록 번호. bsns_rgnmb : ' + bsns_rgnmb } );
-  }
-  res.render('branch', { title: '바이크다 지점 API' });
-});
-
-// Bikeda select Branch list
-router.get('/branches', function( req, res, next ) {
-  db.findAll().then( result => {
-    return res.json( result );
+// 바이크다 지점 전체 목록
+router.get('/branches', util.isLoggedin, function( req, res, next ) {
+  models.branch_office.findAll().then( result => {
+    return res.status(200).json( util.successTrue( result ) );
   }).catch( err => {
-    return res.json( err );
+    return res.status(400).json( util.successFalse( err ) );
   });
-  // var branches = global.branches;
-  // return res.json( branches );
 });
 
-// Bikeda select Branch
-router.get('/branch/:bsns_rgnmb', function( req, res, next ) {
-  var branches = global.branches;
-  var bsns_rgnmb = req.params.bsns_rgnmb;
-  var branch = branches.filter( branch => branch.bsns_rgnmb == bsns_rgnmb );
-  return res.json( branch );
+// 바이크다 지점 조회( 사업자 등록번호, 지점명, 상호, 대표자명 )
+router.get('/branch', util.isLoggedin, function( req, res, next ) {
+  var reqParam = req.query || '';
+  var brcofcBsnsRgnmb = reqParam.brcofcBsnsRgnmb || '';
+  var brcofcNm        = reqParam.brcofcNm || '';
+  var brcofcMtlty     = reqParam.brcofcMtlty || '';
+  var brcofcRprsntvNm = reqParam.brcofcRprsntvNm || '';
+
+  var query = 'select * from tb_branch_offices where 1=1 ';
+  if( brcofcBsnsRgnmb ) query += 'and brcofcBsnsRgnmb like "%' + brcofcBsnsRgnmb + '%" ';
+  if( brcofcNm )        query += 'and brcofcNm like "%' + brcofcNm + '%" ';
+  if( brcofcMtlty )     query += 'and brcofcMtlty like "%' + brcofcMtlty + '%" ';
+  if( brcofcRprsntvNm ) query += 'and brcofcRprsntvNm like "%' + brcofcRprsntvNm + '%" ';
+
+  models.sequelize.query( query ).spread( function ( result, metadata ) {
+    return res.status(200).json( util.successTrue( result ) );
+  }, function ( err ) {
+    return res.status(400).json( util.successFalse( err ) );
+  });
 });
 
-// Bikeda insert branch
-router.post('/branch', function( req, res, next ) {
-  var branches = global.branches;
-  var bsns_rgnmb = req.body.bsns_rgnmb || '';
-  if( bsns_rgnmb.length ){
-    if( branches.filter( branch => branch.bsns_rgnmb == bsns_rgnmb ).length < 1 ) {
-      var branch = {
-        bsns_rgnmb:bsns_rgnmb
-      };
-      global.branches.push(branch);
-      return res.status(201).json(branch);
-    } else {
-      return res.status(400).json({err: '이미 등록된 지점 사업자 등록 번호. bsns_rgnmb : ' + bsns_rgnmb } );
-    }
-  } else {
-    return res.status(400).json({err: '사업자 등록 번호 미입력. bsns_rgnmb'});
-  }
-});
+// 바이크다 지점 등록
+// router.post('/branch', util.isLoggedin, function( req, res, next ) {
+//   var validationError = {
+//     name:'ValidationError',
+//     errors:{}
+//   };
+//
+//   // 입력값 검증
+//   var brcofcBsnsRgnmb = req.body.brcofcBsnsRgnmb || '';
+//
+//   // 지점 등록 여부 검증
+//   models.branch_office.findAll( { where : { brcofcBsnsRgnmb: brcofcBsnsRgnmb } } ).then( result => {
+//     if( result ) return res.status(400).json( {err: '이미 등록된 지점 사업자 등록 번호. brcofcBsnsRgnmb : ' + brcofcBsnsRgnmb } );
+//   }).catch( err => {
+//     return res.status(400).json( util.successFalse( err ) );
+//   });
+//
+//   // 지점 ID 생성
+//   var query = 'select concat("B", lpad(concat(max(cast(substr(brcofcId, 2) AS unsigned))+ 1),4,'0') ) as brcofcId from tb_branch_offices';
+//   var brcofcId = '';
+//   models.sequelize.query( query ).spread( function ( result, metadata ) {
+//     brcofcId = result.brcofcId;
+//   }, function ( err ) {
+//     return res.status(400).json( util.successFalse( err ) );
+//   });
+//
+//   models.branch_office.create()
+//
+//   var brcofcBsnsRgnmb = req.body.brcofcBsnsRgnmb || '';
+//   if( brcofcBsnsRgnmb.length > 0 ){
+//     if( branches.filter( branch => branch.brcofcBsnsRgnmb == brcofcBsnsRgnmb ).length < 1 ) {
+//       var branch = {
+//         brcofcBsnsRgnmb:brcofcBsnsRgnmb
+//       };
+//       return res.status(201).json(branch);
+//     } else {
+//
+//     }
+//   } else {
+//     return res.status(400).json({err: '사업자 등록 번호 미입력. brcofcBsnsRgnmb'});
+//   }
+// });
 
 
 module.exports = router;
